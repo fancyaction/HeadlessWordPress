557094ee84f75cc3cbf16585654e41cdef1fc0b0
fancyaction <labcoatdisaster@gmail.com>
2019-10-06 18:01:28 -0500

    Setup node and templates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,43 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const { allWordpressPage } = useStaticQuery(
+    graphql`
+      query {
+        allWordpressPage {
+          edges {
+            node {
+              id
+              title
+              content
+              excerpt
+              date
+              modified
+              slug
+              status
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <Layout>
+      <div>
+        {allWordpressPage.edges.map(page => (
+          <div key={page.node.id}>
+            <h1>{page.node.title}</h1>
+            <div dangerouslySetInnerHTML={{ __html: page.node.content }} />
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage

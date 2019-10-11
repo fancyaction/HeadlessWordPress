87c9848fb95a7c4116895a9aa08231ca3b8b32c7
fancyaction <labcoatdisaster@gmail.com>
2019-10-11 18:28:22 -0500

    Update portfolio page content

--- src/templates/portfolio.js
+++ src/templates/portfolio.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react"
+import Layout from "../components/layout"
+import styled from "styled-components"
+
+const FeaturedImage = styled.img`
+  max-width: 300px;
+  margin: 16px 0;
+`
 
 export default ({ pageContext }) => (
-    <div>
-        <h1>{pageContext.title}</h1>
-    </div>
+  <Layout>
+    <h1>{pageContext.title}</h1>
+    <FeaturedImage
+      src={pageContext.featuredMedia.source_url}
+      alt={
+        pageContext.featuredMedia.alt_text || pageContext.featuredMedia.title
+      }
+    />
+    <div dangerouslySetInnerHTML={{ __html: pageContext.content }}></div>
+  </Layout>
 )

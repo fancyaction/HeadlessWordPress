a8662ba65d83fdb4531c7c79ba9e7092fa2ba418
fancyaction <labcoatdisaster@gmail.com>
2019-10-29 16:34:07 -0500

    Add wordpress folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
          * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: "2f73c603.ngrok.io",
+        baseUrl: "ea64d3f8.ngrok.io",
         // The protocol. This can be http or https.
         protocol: "http",
         // Indicates whether the site is hosted on wordpress.com.
